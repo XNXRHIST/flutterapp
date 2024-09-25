53f8b520d99c248d1cf2f4eedcdb7cb134b7838e
XNXRHIST <127849618+XNXRHIST@users.noreply.github.com>
2024-09-25 20:11:35 +0300

    Initial commit

---
+++ lib/questions_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/answer_button.dart';
+import 'package:flutter_application_1/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class QuestionsScreen extends StatefulWidget {
+  const QuestionsScreen({super.key, required this.onSelectAnswer});
+
+  final void Function(String answer) onSelectAnswer;
+
+  @override
+  State<QuestionsScreen> createState() {
+    return _QuestionsScreenState();
+  }
+}
+
+class _QuestionsScreenState extends State<QuestionsScreen> {
+  var currentQuestionsIndex = 0;
+
+  void answerQuestion(String selectedAnswers) {
+    widget.onSelectAnswer(selectedAnswers);
+    //currentQuestionsIndex = currentQuestionsIndex + 1;
+    //currentQuestionsIndex += 1;
+    setState(() {
+      currentQuestionsIndex++;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    final currentQuestions = questions[currentQuestionsIndex];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestions.text,
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 201, 153, 251),
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestions.shuffledAnswers.map((answer) {
+              return AnswerButton(
+                answerText: answer,
+                onTap: () {
+                  answerQuestion(answer);
+                },
+              );
+            })
+          ],
+        ),
+      ),
+    );
+  }
+}
